2a87a69b2753835159c179042506d2c744ae61db
HP <HP@Ashish>
2023-09-30 15:52:44 +0530

    Xyz object need to be commented and Abc class object and m2() method called.

---
+++ GitProj_1/src/main/java/com/El/Dao/Abc.java
@@ -0,0 +1,7 @@
+package com.El.Dao;
+
+public class Abc {
+	public void m2() {
+		System.out.println("Method Abc.m2()");
+	}
+}
--- GitProj_1/src/main/java/com/El/model/FirstGit.java
+++ GitProj_1/src/main/java/com/El/model/FirstGit.java
@@ -1,16 +1,19 @@
 package com.El.model;
 
-import com.El.service.Xyz;
+import com.El.Dao.Abc;
 
 public class FirstGit {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GitWorld");
 		System.out.println("Welcome to GitWorld");
-		
-		Xyz xyz =new Xyz();
-		xyz.m1();
 
+		/*
+		 * Xyz xyz =new Xyz(); xyz.m1();
+		 */
+
+		Abc abc = new Abc();
+		abc.m2();
 	}
 
 }
