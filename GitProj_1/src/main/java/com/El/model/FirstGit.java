3d7682d10c1c85ebef799040b00e63471e19be4e
HP <HP@Ashish>
2023-09-30 15:32:25 +0530

    object created in First Class and m1() method call performed.

--- GitProj_1/src/main/java/com/El/model/FirstGit.java
+++ GitProj_1/src/main/java/com/El/model/FirstGit.java
@@ -1,10 +1,15 @@
 package com.El.model;
 
+import com.El.service.Xyz;
+
 public class FirstGit {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GitWorld");
 		System.out.println("Welcome to GitWorld");
+		
+		Xyz xyz =new Xyz();
+		xyz.m1();
 
 	}
 
