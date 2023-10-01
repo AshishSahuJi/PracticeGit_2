88a8b017b4b3406b5904fb465e557b76f115ad83
HP <HP@Ashish>
2023-10-01 14:13:22 +0530

    Tl-commit

--- GitProj_1/src/main/java/com/El/Dao/Abc.java
+++ GitProj_1/src/main/java/com/El/Dao/Abc.java
@@ -2,10 +2,7 @@ package com.El.Dao;
 
 public class Abc {
 	public void m2() {
-<<<<<<< HEAD
-		System.out.println("Method Abc.m2() method fr testing");
-=======
+
 		System.out.println("Method Abc.m2()for kerasuhg");
->>>>>>> branch 'master' of https://github.com/AshishSahuJi/PracticeGit_2.git
 	}
 }
